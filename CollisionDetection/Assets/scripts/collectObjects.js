5c6207111b6dbc3c04b23fa6ce183ed80006d433
kjetilfjellheim <kjetil@forgottendonkey.net>
2017-06-24 00:46:05 +0200

    First version of collision detection

---
+++ CollisionDetection/Assets/scripts/collectObjects.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+import UnityEngine.SceneManagement;
+
+var score:int = 0;
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+
+function OnControllerColliderHit(hit: ControllerColliderHit) {
+    if (hit.collider.gameObject.tag == "pick_me") {
+        score++;
+        Destroy(hit.collider.gameObject);
+    }
+    if (score == 4) {
+        SceneManagement.SceneManager.LoadScene("collisionScene");
+    }
+}
