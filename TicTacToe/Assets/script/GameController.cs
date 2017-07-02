2c2a2903184a3fe42da7804f53ad0f9063fd9287
kjetilfjellheim <kjetil@forgottendonkey.net>
2017-07-02 16:17:24 +0200

    Add tic tac toe

---
+++ TicTacToe/Assets/script/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Button[] buttonList;
+    public Text currentPlayer;
+
+    public void Start()
+    {
+        foreach (Button button in buttonList)
+        {
+            button.onClick.AddListener(delegate { clickEvent(button); });
+        }
+    }
+
+    public void clickEvent(Button eventButton)
+    {
+        string currentValue = eventButton.GetComponentInChildren<Text>().text;
+        if (eventButton.interactable == true && currentValue.Equals(""))
+        {
+            eventButton.GetComponentInChildren<Text>().text = currentPlayer.text;
+            nextPlayer();
+            eventButton.interactable = false;
+        } 
+    }
+
+    public void nextPlayer()
+    {
+        if (currentPlayer.text.Equals("X"))
+        {
+            currentPlayer.text = "O";
+        }
+        else
+        {
+            currentPlayer.text = "X";
+        }
+
+    }
+
+
+}
