0c0a8eac86cfc97cffe2ea55e4102fad9c3e961d
kjetilfjellheim <kjetil@forgottendonkey.net>
2017-07-02 04:09:01 +0200

    Added spash screen example

---
+++ SpashScreen/Assets/scripts/MoveOn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MoveOn : MonoBehaviour {
+
+    public Button MoveOnBtn;
+
+    // Use this for initialization
+    void Start () {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(() => { MoveToNextScene(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void MoveToNextScene()
+    {
+        Application.LoadLevel("mainscene");
+    }
+
+}
