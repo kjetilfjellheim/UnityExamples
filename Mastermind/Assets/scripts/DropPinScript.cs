6c3ddfc6dfce42ec4a3b95cacbde7e94c8d77885
kjetilfjellheim <kjetil@forgottendonkey.net>
2017-07-12 02:10:32 +0200

    Finished implementing first version

---
+++ Mastermind/Assets/scripts/DropPinScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DropPinScript : MonoBehaviour, IDropHandler {
+
+	public Image image;
+
+	public void OnDrop(PointerEventData data) 
+	{
+		if (data.pointerDrag != null) 
+		{
+			GameObject gameObject = data.pointerDrag;
+			Image pinImage = gameObject.GetComponentInChildren<Image> ();
+			image.sprite = pinImage.sprite;
+
+		}
+	}
+
+}
--- Mastermind/Assets/scripts/ResultScript.cs
+++ Mastermind/Assets/scripts/ResultScript.cs
@@ -5,15 +5,21 @@ using UnityEngine.UI;
 
 public class ResultScript : MonoBehaviour {
 
+	public const int MAX_GUESSES = 10;
+
+	public const int COLOR_GUESSES = 4;
+
     public enum Color { BLUE, CYAN, RED, GREEN, PURPLE, YELLOW };
-
-    public System.Random random = new System.Random();
-
-    public Color[] resultColors = new Color[4];
 
     public Button newGameBtn;
 
-    public Image[] guessResult;
+	private Sprite[] resultSprites = new Sprite[COLOR_GUESSES];
+
+	public Sprite emptyResultImage;
+
+	public Sprite emptyImage;
+
+	public Image[] guessResult;
 
     public Image blueImage;
     public Image purpleImage;
@@ -22,18 +28,21 @@ public class ResultScript : MonoBehaviour {
     public Image yellowImage;
     public Image greenImage;
 
-    private int guessNumber = 0;
+	public Sprite correctPlaceResultImage;
+	public Sprite wrongPlaceResultImage;
 
-    public Button[] guessButton;
+    private int guessNumber = 1;
 
-    // Use this for initialization
+    public Button guessButton;
+
     void Start ()
     {
+        Debug.Log("Start.");
         newGameBtn.onClick.AddListener(delegate { NewGame(); });
-        Debug.Log("Start resultscript");
+		guessButton.onClick.AddListener(delegate { Guess(); });
+		NewGame ();
     }
 	
-	// Update is called once per frame
 	void Update ()
     {
 		
@@ -42,40 +51,178 @@ public class ResultScript : MonoBehaviour {
     public void NewGame()
     {
         Debug.Log("New game");
-        resultColors = new Color[4];
-        for (int i = 0; i < 4;i++) { 
-            System.Array values = System.Enum.GetValues(typeof(Color));
-            Color color = (Color)values.GetValue(random.Next(values.Length));
-            resultColors[i] = color;
-            if (color == Color.BLUE)
-            {
-                guessResult[i].sprite = blueImage.sprite;
-            }
-            else if (color == Color.CYAN)
-            {
-                guessResult[i].sprite = cyanImage.sprite;
-            }
-            else if (color == Color.GREEN)
-            {
-                guessResult[i].sprite = greenImage.sprite;
-            }
-            else if (color == Color.PURPLE)
-            {
-                guessResult[i].sprite = purpleImage.sprite;
-            }
-            else if (color == Color.RED)
-            {
-                guessResult[i].sprite = redImage.sprite;
-            }
-            else
-            {
-                guessResult[i].sprite = yellowImage.sprite;
-            }
-        } 
-        for (int i = 0;i < 10; i++)
-        {
-            guessButton[i].enabled = false;
-        }
-    } 
+		ResetBoard();
+    }
+
+    public void Guess()
+    {
+		if (CanGuess()) 
+		{
+	        Debug.Log("Guessing");
+			bool solved = SetResultPins();
+			if (solved) 
+			{
+				ShowResult ();
+				HideGuessButton ();
+			}
+	        guessNumber++;        
+			if (guessNumber > MAX_GUESSES) {   
+				ShowResult ();
+				HideGuessButton ();
+			} else {
+				SetGuessButtonPosition ();
+			}
+			SetAllowPinDrag ();
+		}
+    }
+
+	public void ShowResult()
+    {
+        Debug.Log("Shows result.");
+		for (int i = 0; i< COLOR_GUESSES; i++) 
+		{
+			guessResult [i].sprite = resultSprites [i];
+		}
+    }
+
+	public void SetGuessButtonPosition() 
+	{
+		float newY = GameObject.Find("Panel" + guessNumber).transform.position.y;
+		Vector3 vector = guessButton.transform.position;
+		vector.y = newY;
+		guessButton.transform.position = vector;
+	}
+
+	public bool CanGuess() 
+	{
+		if (guessNumber > MAX_GUESSES || !HasFilledPins()) 
+		{
+			return false;
+		}
+		return true;
+	}
+
+	public void ResetBoard()
+	{
+		System.Random random = new System.Random ();
+		for (int i = 0; i < COLOR_GUESSES;i++) { 
+			System.Array values = System.Enum.GetValues(typeof(Color));
+			Color color = (Color)values.GetValue(random.Next(values.Length));
+			this.guessResult [i].sprite = this.emptyImage;
+			if (color == Color.BLUE)
+			{								
+				resultSprites[i] = blueImage.sprite;
+			}
+			else if (color == Color.CYAN)
+			{
+				resultSprites[i] = cyanImage.sprite;
+			}
+			else if (color == Color.GREEN)
+			{
+				resultSprites[i] = greenImage.sprite;
+			}
+			else if (color == Color.PURPLE)
+			{
+				resultSprites[i] = purpleImage.sprite;
+			}
+			else if (color == Color.RED)
+			{
+				resultSprites[i] = redImage.sprite;
+			}
+			else
+			{
+				resultSprites[i] = yellowImage.sprite;
+			}
+		}
+		for (int panelCnt = 1;panelCnt <= MAX_GUESSES; panelCnt++) 
+		{
+			for (int pinCnt = 1;pinCnt <= COLOR_GUESSES; pinCnt++ ) {
+				GameObject.Find ("Choice_" + panelCnt + "_" + pinCnt).GetComponentInChildren<Image>().sprite = emptyImage;
+				GameObject.Find ("Result_" + panelCnt + "_" + pinCnt).GetComponentInChildren<Image>().sprite = emptyImage;
+			}
+		}
+		guessNumber = 1;
+		SetGuessButtonPosition ();		
+		SetAllowPinDrag ();
+	}	
+
+	public void HideGuessButton() 
+	{
+		float newY = GameObject.Find ("ResultPanel").transform.position.y + 1000;
+		Vector3 vector = guessButton.transform.position;
+		vector.y = newY;
+		guessButton.transform.position = vector;
+	}
+
+	public bool HasFilledPins()
+	{
+		int pinsFilled = 0;
+		for (int i = 1; i <= COLOR_GUESSES; i++) 
+		{
+			if (!GameObject.Find ("Choice_" + this.guessNumber + "_" + i).GetComponentInChildren<Image> ().sprite.Equals(emptyImage)) 
+			{
+				pinsFilled++;
+			}
+		}
+		return (pinsFilled == COLOR_GUESSES);
+	}
+
+	public bool SetResultPins()
+	{
+		int numWhite = 0;
+		int numBlack = 0;
+		bool[] guessUsedPins = new bool[COLOR_GUESSES]{false, false, false, false};
+		bool[] resultUsedPins = new bool[COLOR_GUESSES]{false, false, false, false};
+		for (int pinCnt = 1; pinCnt <= COLOR_GUESSES; pinCnt++) 
+		{
+			Sprite guessSprite = GameObject.Find ("Choice_" + this.guessNumber + "_" + pinCnt).GetComponentInChildren<Image> ().sprite;
+			if (guessSprite.Equals(resultSprites[pinCnt - 1])) 
+			{
+				numBlack++;
+				guessUsedPins [pinCnt - 1] = true;
+				resultUsedPins [pinCnt - 1] = true;
+			}
+		}
+		for (int guessPinCnt = 1; guessPinCnt <= COLOR_GUESSES; guessPinCnt++) 
+		{
+			for (int resultPinCnt = 1; resultPinCnt <= COLOR_GUESSES; resultPinCnt++) 
+			{
+				if (!resultUsedPins[resultPinCnt - 1] && !guessUsedPins[guessPinCnt - 1])  
+				{
+					Sprite guessSprite = GameObject.Find ("Choice_" + this.guessNumber + "_" + guessPinCnt).GetComponentInChildren<Image> ().sprite;
+					if (guessSprite.Equals (resultSprites [resultPinCnt - 1])) 
+					{
+						guessUsedPins [guessPinCnt - 1] = true;
+						resultUsedPins [resultPinCnt - 1] = true;
+						numWhite++;
+					}
+				}
+			}
+		}
+		for (int blackPinCnt = 1; blackPinCnt <= numBlack; blackPinCnt++) 
+		{
+			Image resultSprite = GameObject.Find ("Result_" + this.guessNumber + "_" + blackPinCnt).GetComponentInChildren<Image> ();
+			resultSprite.sprite = this.correctPlaceResultImage;
+		}
+		for (int whitePinCnt = (numBlack + 1); whitePinCnt <= (numBlack + numWhite); whitePinCnt++) 
+		{
+			Image resultSprite = GameObject.Find ("Result_" + this.guessNumber + "_" + whitePinCnt).GetComponentInChildren<Image> ();
+			resultSprite.sprite = this.wrongPlaceResultImage;		
+		}
+		Debug.Log ("guessNumber black: " + numBlack);
+		Debug.Log ("guessNumber white: " + numWhite);
+		return (numBlack == COLOR_GUESSES);
+	}
+
+	public void SetAllowPinDrag() 
+	{
+		for (int panelCnt = 1;panelCnt <= MAX_GUESSES; panelCnt++) 
+		{
+			for (int pinCnt = 1;pinCnt <= COLOR_GUESSES; pinCnt++ ) {
+				GameObject.Find ("Choice_" + panelCnt + "_" + pinCnt).GetComponentInChildren<Image>().raycastTarget = (this.guessNumber == panelCnt);
+			}
+		}
+	}
 
 }
+
